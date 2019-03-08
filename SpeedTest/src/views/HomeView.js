798f622308942240e2d1db23f92c778b92c5e5d8
David Thibault <dthibault@medial.ca>
2019-03-08 14:00:09 -0500

    wip

--- SpeedTest/src/views/HomeView.js
+++ SpeedTest/src/views/HomeView.js
@@ -7,51 +7,80 @@ class HomeView extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            working: false,
+            doingPing: false,
             totalSize: 0,
             progressDownload : 0,
             progressUpload: 0,
             speed: null,
             pingTime :null
         };
-        this.handleGet = this.handleGet.bind(this);
-        this.handlePost = this.handlePost.bind(this);
+        this.handleStart = this.handleStart.bind(this);
+
     }
+
 
     componentDidMount() {
 
     }
 
-    handleGet() {
-        var start = Date.now(), elapsed, end, totalSize = 0;
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.something !== prevState.something) {
+            this.props.somefunction(this.state.something); 
+        }
+    }
 
-        var p = new Ping();
-        p.ping("https://lecampus.com/", (err, data) => {
-            this.setState({ pingTime: data });
-        });
+    doPing() {
+        var p = new Ping(), total = 0, occurence = 0;
+        for (var i = 0; i < 5; i++) {
+            setTimeout(() => {
+                p.ping("https://lecampus.com/", (err, data) => {
+                    if (!err) {
+                        occurence++;
+                        total += data;
+                        this.setState({ pingTime: total / occurence });
+                    }
+                });
+            }, 1000);
+        }
+    }
 
-        axios.get('test.jpg?n=' + Math.random, {
+    doDownload() {
+        axios.get('test.jpg?n=' + Math.random(), {
             onDownloadProgress: (progressEvent) => {
                 this.setState({ totalSize: progressEvent.total / 1000000 });
                 this.setState({ progressDownload: progressEvent.loaded / progressEvent.total * 100 });
             }
         }).then(() => {
             var end = Date.now();
-            var elapsed = (end - start) /1000; 
+            var elapsed = (end - start) / 1000;
             this.setState({ speed: this.state.totalSize / elapsed * 8 });
+            this.setState({ working: false });
         })
         .catch(function (error) {
             console.log('error' + error);
         });
     }
 
-    handlePost() {
-        axios.post('test', 'none')
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+    doUpload() {
+                //axios.post('test', 'none')
+        //    .then(function (response) {
+        //        console.log(response);
+        //    })
+        //    .catch(function (error) {
+        //        console.log(error);
+        //    });
+    }
+
+    handleStart() {
+        if (this.state.working) {
+            return;
+        }
+        this.setState({ working: true });
+        var start = Date.now(), elapsed, end, totalSize = 0;
+
+        this.doPing();
+
     }
     
     render() {
@@ -60,8 +89,7 @@ class HomeView extends Component {
         return (
             <div>
                 <Line percent={progressDownload} strokeWidth="1" strokeColor="#3FC7FA" />
-                <button onClick={this.handleGet}>Tester votre vitesse</button>
-                {/*<button onClick={this.handlePost}>Post</button>*/}
+                <button onClick={this.handleStart}>Tester votre vitesse</button>
                 {pingTime ? <p>votre PING est {pingTime} millisecondes</p> : null }
                 {speed ? <p>votre vitesse est de {speed} mbps</p>: null}
             </div>

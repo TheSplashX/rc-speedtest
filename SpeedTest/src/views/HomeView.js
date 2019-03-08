83e061240eba431a5bdd723db0af81a0b0c53cdf
David Thibault <dthibault@medial.ca>
2019-03-08 10:33:14 -0500

    Fix dependencies + added ping.js
    unused React dependencies removed and added ping functionality with ping.js

--- SpeedTest/src/views/HomeView.js
+++ SpeedTest/src/views/HomeView.js
@@ -1,6 +1,7 @@
 ﻿import React, { Component } from 'react';
 import axios from 'axios';
-import { Line } from 'rc-progress'; //https://react-component.github.io/progress/examples/simple.html
+import { Line } from 'rc-progress'; // https://react-component.github.io/progress/examples/simple.html
+import Ping from 'ping.js'; // https://github.com/alfg/ping.js
 
 class HomeView extends Component {
     constructor(props) {
@@ -9,7 +10,8 @@ class HomeView extends Component {
             totalSize: 0,
             progressDownload : 0,
             progressUpload: 0,
-            speed: null
+            speed: null,
+            pingTime :null
         };
         this.handleGet = this.handleGet.bind(this);
         this.handlePost = this.handlePost.bind(this);
@@ -21,6 +23,12 @@ class HomeView extends Component {
 
     handleGet() {
         var start = Date.now(), elapsed, end, totalSize = 0;
+
+        var p = new Ping();
+        p.ping("https://lecampus.com/", (err, data) => {
+            this.setState({ pingTime: data });
+        });
+
         axios.get('test.jpg?n=' + Math.random, {
             onDownloadProgress: (progressEvent) => {
                 this.setState({ totalSize: progressEvent.total / 1000000 });
@@ -47,13 +55,14 @@ class HomeView extends Component {
     }
     
     render() {
-        const { progressDownload, speed} = this.state;
+        const { progressDownload, speed, pingTime } = this.state;
 
         return (
             <div>
                 <Line percent={progressDownload} strokeWidth="1" strokeColor="#3FC7FA" />
                 <button onClick={this.handleGet}>Tester votre vitesse</button>
                 {/*<button onClick={this.handlePost}>Post</button>*/}
+                {pingTime ? <p>votre PING est {pingTime} millisecondes</p> : null }
                 {speed ? <p>votre vitesse est de {speed} mbps</p>: null}
             </div>
         );

d98414023d34be15da2074d40e39a97577a699b2
TheSplashX <dr.splash@hotmail.com>
2019-03-09 09:28:51 -0500

    Create dummy file if not present
    Create dummy file if not present in the wwwroot ("test.jpg"), checked on startup

---
+++ SpeedTest/api/utils/ShellHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace SpeedTest.api.utils
+{
+    /// <summary>
+    /// see https://stackoverflow.com/a/1881076/6296782
+    /// </summary>
+    public static class ShellHelper
+    {
+        public static void CreateDummy(string webroot)
+        {
+            var file = Path.Combine(webroot, "test.jpg");
+            using (var fs = new FileStream(file, FileMode.CreateNew))
+            {
+                fs.Seek(50L * 1024 * 1024, SeekOrigin.Begin);
+                fs.WriteByte(0);
+            }
+        }
+    }
+}

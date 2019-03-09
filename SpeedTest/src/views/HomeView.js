71cc3729bc935a40ea3f49dc49d4469d49acb01f
TheSplashX <dr.splash@hotmail.com>
2019-03-08 23:42:06 -0500

    WIP
    Removed unused modules and components
    Added Fix for async in .babelrc
    Added utils module
    Added ping precision

--- SpeedTest/src/views/HomeView.js
+++ SpeedTest/src/views/HomeView.js
@@ -2,13 +2,13 @@
 import axios from 'axios';
 import { Line } from 'rc-progress'; // https://react-component.github.io/progress/examples/simple.html
 import Ping from 'ping.js'; // https://github.com/alfg/ping.js
+import { delay, asyncSetState } from '../components/utils';
 
 class HomeView extends Component {
     constructor(props) {
         super(props);
         this.state = {
             working: false,
-            doingPing: false,
             totalSize: 0,
             progressDownload : 0,
             progressUpload: 0,
@@ -16,36 +16,10 @@ class HomeView extends Component {
             pingTime :null
         };
         this.handleStart = this.handleStart.bind(this);
-
-    }
-
-
-    componentDidMount() {
-
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if (this.state.something !== prevState.something) {
-            this.props.somefunction(this.state.something); 
-        }
-    }
-
-    doPing() {
-        var p = new Ping(), total = 0, occurence = 0;
-        for (var i = 0; i < 5; i++) {
-            setTimeout(() => {
-                p.ping("https://lecampus.com/", (err, data) => {
-                    if (!err) {
-                        occurence++;
-                        total += data;
-                        this.setState({ pingTime: total / occurence });
-                    }
-                });
-            }, 1000);
-        }
     }
 
     doDownload() {
+        var start = Date.now(), elapsed, end, totalSize = 0;
         axios.get('test.jpg?n=' + Math.random(), {
             onDownloadProgress: (progressEvent) => {
                 this.setState({ totalSize: progressEvent.total / 1000000 });
@@ -72,15 +46,33 @@ class HomeView extends Component {
         //    });
     }
 
-    handleStart() {
+    async doPing() {
+        let p = new Ping(), total = 0, occurence = 0;
+        console.log('beginning ping test');
+        async function loop() {
+            for (let i = 0; i < 6; i++) {
+                p.ping("http://lecampus.com/", (err, data) => {
+                    if (!err && !isNaN(data) && i > 0 ) {
+                        occurence++;
+                        total += data;  
+                        console.log('ping test #'+ i +' of ' + data + 'ms');
+                    }
+                });
+                await delay(1000);
+            }
+        }
+        await loop();
+        await asyncSetState(this)({ pingTime: total / occurence });
+        console.log('finished ping test');
+    }
+
+    async handleStart() {
         if (this.state.working) {
             return;
         }
-        this.setState({ working: true });
-        var start = Date.now(), elapsed, end, totalSize = 0;
-
-        this.doPing();
-
+        await asyncSetState(this)({ working: true });
+        await this.doPing();
+        await asyncSetState(this)({ working: false });
     }
     
     render() {
@@ -90,7 +82,7 @@ class HomeView extends Component {
             <div>
                 <Line percent={progressDownload} strokeWidth="1" strokeColor="#3FC7FA" />
                 <button onClick={this.handleStart}>Tester votre vitesse</button>
-                {pingTime ? <p>votre PING est {pingTime} millisecondes</p> : null }
+                {pingTime ? <p>votre PING est {Math.round(pingTime)} millisecondes</p> : null }
                 {speed ? <p>votre vitesse est de {speed} mbps</p>: null}
             </div>
         );
